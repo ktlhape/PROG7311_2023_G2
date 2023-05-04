2c38ad07631a406355abdd47d8f6f52cab2aa934
ktlhape <ktlhape@iie.ac.za>
2023-05-04 20:46:41 +0200

    Connect MVC to Database

---
+++ ConnectToDB/ConnectToDB/Controllers/StudentController.cs
@@ -0,0 +1,113 @@
+﻿using ConnectToDB.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
+
+namespace ConnectToDB.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string con;
+        private IConfiguration _config;
+
+        public StudentController(IConfiguration configuration)
+        {
+            _config = configuration;
+            con = _config.GetConnectionString("dbConnect");
+        }
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            List<Student> stList = new List<Student>();
+            SqlConnection myConnection = new SqlConnection(con);
+            SqlDataAdapter cmdSelect = new SqlDataAdapter($"SELECT * FROM Student", myConnection    );
+            DataTable dt = new DataTable();
+            DataRow dr;
+            myConnection.Open();
+            cmdSelect.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    dr = dt.Rows[i];
+                    stList.Add(new Student((string)dr[0], (string)dr[1], (string)dr[2]));
+                }
+            }
+            myConnection.Close();
+            return View(stList);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ConnectToDB/ConnectToDB/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace ConnectToDB.Models
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Student()
+        {
+            this.StudentID = "";
+            this.Name = "No name";
+            this.Surname = "No surname";
+        }
+        public Student(string studentID, string name, string surname)
+        {
+            StudentID = studentID;
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
